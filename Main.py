be2e05a0d2bafa2836e9d0a67e4c984e0d0f364a
Manasvi Pant <88909122+ManasviPant09@users.noreply.github.com>
2021-08-17 03:20:34 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,3 +3,5 @@ import png
 from pyqrcode import QRCode
 url = pyqrcode.create('http://en.wikipedia.org')
 url.png('Wikipedia-Url.png',scale = 10)
+url = pyqrcode.create('http://uca.edu')
+url.svg('uca-url.svg', scale=8)
